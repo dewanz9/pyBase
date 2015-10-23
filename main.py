2a16af90441287bf52765731fbe18153d0b05b2c
Connor Dewar <connor.dewar@gmail.com>
2015-10-23 13:49:01 +1300

    Update main.py
    fixed shit

--- main.py
+++ main.py
@@ -1,13 +1,7 @@
+import os
 """
 text document format 
-
 ID,first,last,age
-
-
-
-
-
-
 """
 
 def createDataBase(name,*columns):
@@ -17,8 +11,38 @@ def createDataBase(name,*columns):
             stringToWrite = item
         else:
             stringToWrite = stringToWrite+","+item
-    database = open(name+".txt","w")
-    database.write(stringToWrite+"\n")
-    database.close()
+    if os.path.exists("./" + name + ".txt"):
+        print "ERROR- database allready exists"
+    else:
+        database = open("./" + name + ".txt","w")
+        database.write(stringToWrite)
+        database.close()
+    
+class workingBase(object):
+    def __init__(self,name):
+        self.filename = "./" + name + ".txt"
+        if os.path.exists(self.filename):
+            self.dataBase = open(self.filename)
+            print "database opened successfully"
+            test = self.dataBase.readline() 
+            self.columns = test.split(",")
+            self.dataBase.close()
+        else:
+            print "ERROR- database doesnt exist"
+        
+            
+    def addEntry(self,*fields):
+        if len(fields) < len(self.columns):
+            print "not enough columns to add entry"
+            
+        elif len(fields) > len(self.columns):
+            print "too many columns to add entry"
+                
+        else:
+            pass
+    
+
     
 createDataBase("swag","first","last","age")
+swag = workingBase("swag")
+swag.addEntry("sean","connery",14)
